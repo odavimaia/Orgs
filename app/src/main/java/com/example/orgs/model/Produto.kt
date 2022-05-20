8e5485e6201f6e379ca21315036fe2b12011a437
odavimaia <daviemmcm@gmail.com>
2022-05-20 11:07:28 -0400

    Classe produto consertada e formulário de cadastro de produto implementado

--- app/src/main/java/com/example/orgs/model/Produto.kt
+++ app/src/main/java/com/example/orgs/model/Produto.kt
@@ -2,8 +2,8 @@ package com.example.orgs.model
 
 import java.math.BigDecimal
 
-class Produto {
+data class Produto (
     val nome: String,
     val descricao: String,
     val valor: BigDecimal
-}
+    )
